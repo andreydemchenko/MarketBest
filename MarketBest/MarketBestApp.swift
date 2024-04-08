7208f7a0d61979d2a4d5d1f8ae5c32c996a7e2dd
andreydem <andreydem906.082@gmail.com>
2024-04-08 15:33:13 +0300

    Initial Commit

---
+++ MarketBest/MarketBestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketBestApp.swift
+//  MarketBest
+//
+//  Created by Macbook Pro on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarketBestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
